633bb1949c4e60d0057249e331ddc9cb1d62d9e6
luis carlos <luisestrela.bol951@gmail.com>
2020-03-28 01:33:14 -0300

    create header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import Header from './components/Header';
+
 import GlobalStyle from './styles/GlobalStyles';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <Header />
       <GlobalStyle />
     </BrowserRouter>
   );
